23d216b51fe1d2ca24f55881a0874b66f9107b43
Hancminnah <minpopoo@gmail.com>
2025-09-30 14:27:50 +0800

    changes to time series analysis

--- Time_Series_Analysis/time_series_analysis.py
+++ Time_Series_Analysis/time_series_analysis.py
@@ -8,6 +8,7 @@ import copy
 from plotnine import ggplot, aes, geom_line, geom_point, facet_wrap, labs, theme_bw
 from plotnine import ggsave
 from plotnine import scale_x_datetime
+from sklearn.metrics import mean_squared_error
 
 
 # Process Data
@@ -26,23 +27,6 @@ df = pd.get_dummies(df, columns=['completion_status'],drop_first=False)
 times_series_df = df.groupby(['course_name','semester_num'])['completion_status_failed'].apply(lambda x: (x.sum() / x.count())*100).reset_index()
 times_series_df['semester'] = df['semester'].replace({'2020-01-01':'Fall 2020', '2020-01-02':'Spring 2021', '2020-01-03':'Fall 2021', '2020-01-04':'Spring 2022','2020-01-05':'Fall 2022','2020-01-06':'Spring 2023'})
 
-# fit an ARIMA model and plot residual errors
-design_thinking = times_series_df[times_series_df['course_name']=='Design Thinking'][['semester','completion_status_failed']]
-design_thinking = design_thinking.set_index('semester')
-design_thinking.index.freq = 'semester'
-
-engineering_basics = times_series_df[times_series_df['course_name']=='Engineering Basics'][['semester','completion_status_failed']]
-engineering_basics = engineering_basics.set_index('semester')
-engineering_basics.index.freq = 'semester'
-
-intro_cs = times_series_df[times_series_df['course_name']=='Intro to CS'][['semester','completion_status_failed']]
-intro_cs = intro_cs.set_index('semester')
-intro_cs.index.freq = 'semester'
-
-prin_business = times_series_df[times_series_df['course_name']=='Principles of Business'][['semester','completion_status_failed']]
-prin_business = prin_business.set_index('semester')
-prin_business.index.freq = 'semester'
-
 plot_df = times_series_df.copy()
 # Ensure 'semester' is ordered categorically for proper plotting
 semester_order = ['Fall 2020', 'Spring 2021', 'Fall 2021', 'Spring 2022', 'Fall 2022', 'Spring 2023']
@@ -60,17 +44,34 @@ p = (
 ggsave(p, filename='results/failure_rate_by_course_and_semester.png', verbose=False)
 print(p)
 
+times_series_df.to_csv('./results/time_series_data.csv', index=False)
 
-# pyplot.plot(design_thinking.index,design_thinking, linestyle='--', marker='o', color='k')
-# pyplot.plot(engineering_basics.index,engineering_basics, linestyle='--', marker='o', color='b')
-# pyplot.plot(intro_cs.index,intro_cs, linestyle='--', marker='o', color='r')
-# pyplot.plot(prin_business.index,prin_business, linestyle='--', marker='o', color='g')
-# pyplot.xlabel('Semester')
-# pyplot.ylabel('Percentage of students who failed')
-# # Add legend
-# pyplot.legend(['Design Thinking','Engineering Basics','Intro to CS','Principles of Business'])
-# pyplot.show()
+# Check for stationarity using Dickey-Fuller Test
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.ar_model import AutoReg
+from plotnine import geom_ribbon
+def check_stationarity(timeseries):
+    result = adfuller(timeseries)
+    print('Dickey-Fuller Test:')
+    print(f'ADF Statistic: {result[0]}')
+    print(f'p-value: {result[1]}')
+    for key, value in result[4].items():
+        print(f'Critical Value {key}: {value}')
+    if result[1] <= 0.05:
+        print("The time series is stationary.")
+    else:
+        print("The time series is non-stationary.")
 
+check_stationarity(times_series_df[times_series_df['course_name']=='Design Thinking']['completion_status_failed'])
+# Since the series is not stationary, difference it to make stationary
+design_thinking_series = times_series_df[times_series_df['course_name']=='Design Thinking']['completion_status_failed']
+design_thinking_diff = design_thinking_series.diff().dropna()
+check_stationarity(design_thinking_diff)
+
+
+
+
+# Note: The ARIMA model fitting and forecasting code is not included here as the dataset is small and primarily for demonstration.
 # References:
 # 1. https://www.datacamp.com/tutorial/arima
 # 2. https://stackoverflow.com/questions/62783633/how-to-interpret-plots-of-autocorrelation-and-partial-autocorrelation-using-pyth
@@ -78,3 +79,4 @@ print(p)
 # 4. https://people.duke.edu/%7Ernau/411arim3.htm
 # 5. https://www.geeksforgeeks.org/machine-learning/time-series-analysis-and-forecasting/
 # 6. https://www.geeksforgeeks.org/machine-learning/python-arima-model-for-time-series-forecasting/
+# 7. https://statpowers.com/timeSeries.html
